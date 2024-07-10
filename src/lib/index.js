25cce15026b898e256bf328bfb7480117535e975
tree <tree@hidden.cz>
2024-07-10 17:41:49 +0200

    update data path

--- src/lib/index.js
+++ src/lib/index.js
@@ -6,7 +6,7 @@ export {
 }*/
 
 export async function loadData () {
-    const res = await fetch(`https://data.bsky.cz/data.json`);
+    const res = await fetch(`https://data.bsky.cz/index.json`);
 	const data = await res.json();
     return data
 }
